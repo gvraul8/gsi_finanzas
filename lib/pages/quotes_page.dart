6c55a979dc51c7077b63cdbc0a885cf10ec30038
Raúl González Velázquez <raul.gonzalez15@alu.uclm.es>
2023-04-16 18:14:38 +0200

    actualizacion de precios cada 30 seg

--- lib/pages/quotes_page.dart
+++ lib/pages/quotes_page.dart
@@ -1,35 +1,77 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:gsi_finanzas/models/quote_model.dart';
 import 'package:gsi_finanzas/services/finnhub_service.dart';
-
 import '../components/quotes_list_tile.dart';
 
-class QuotesPage extends StatelessWidget {
+class QuotesPage extends StatefulWidget {
   const QuotesPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final FinnhubService client = FinnhubService();
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF020060),
-          title: const Text('Precios'),
-        ),
-        body: FutureBuilder(
-          future: client.getQuotes(),
-          builder: (BuildContext context, AsyncSnapshot<List<Quote>> snapshot) {
-            if (snapshot.hasData) {
-              List<Quote>? quotes = snapshot.data;
-              return ListView.builder(
-                  itemCount: quotes?.length,
-                  itemBuilder: (context, index) =>
-                      QuotesListTile(quotes![index], context));
-            }
+  _QuotesPageState createState() => _QuotesPageState();
+}
 
-            return const Center(
+class _QuotesPageState extends State<QuotesPage> {
+  late Timer _timer;
+  List<Quote>? _quotes;
+  final FinnhubService _client = FinnhubService();
+  final DateFormat _dateFormat = DateFormat('dd/MM/yyyy'); // Formato de fecha
+  final DateFormat _timeFormat = DateFormat('HH:mm:ss'); // Formato de hora
+  String currentDate = '';
+  String currentTime = '';
+
+  @override
+  void initState() {
+    _getQuotes(); // Obtener los datos al iniciar el widget
+    _timer = Timer.periodic(const Duration(seconds: 30), (_) {
+      // Actualizar los datos cada 30 segundos
+      _getQuotes();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel(); // Cancelar el timer cuando el widget se dispose
+    super.dispose();
+  }
+
+  Future<void> _getQuotes() async {
+    final List<Quote>? quotes = await _client.getQuotes();
+
+    setState(() {
+      _quotes = quotes;
+      currentDate = _dateFormat.format(DateTime.now());
+      currentTime = _timeFormat.format(DateTime.now());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF020060),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Precios'),
+            Text(
+              'Última actualización: $currentDate $currentTime',
+              style: const TextStyle(fontSize: 10),
+            ),
+          ],
+        ),
+      ),
+      body: _quotes != null
+          ? ListView.builder(
+              itemCount: _quotes!.length,
+              itemBuilder: (context, index) =>
+                  QuotesListTile(_quotes![index], context),
+            )
+          : const Center(
               child: CircularProgressIndicator(),
-            );
-          },
-        ));
+            ),
+    );
   }
 }
